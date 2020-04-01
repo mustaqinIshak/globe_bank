ee35d0d84b693ac4ab15b28179d0b386407f62b9
mustaqinIshak <mustaqinishak@gmail.com>
2020-04-01 20:22:47 +0800

    moce file css to folder public

--- public/staff/index.php
+++ public/staff/index.php
@@ -1,6 +1,8 @@
 <?php require_once("../../private/initialize.php"); ?>
 <?php 
     $page_title= "Staff menu";
+    $var = "takin";
+    echo substr($var, 5, 10);
 ?>
 
 <?php include(SHARED_PATH ."/staff_header.php"); ?>
